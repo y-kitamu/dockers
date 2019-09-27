0122ae55927ada1b724afb8f2c13881d2d77c200
Kitamura <ymyk6602@gmail.com>
2019-09-27 12:00:20 +0900

    add jupyter lab

---
+++ anaconda/config/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ENTRYPOINT命令はコンテナ起動時に実行するコマンドを指定（基本docker runの時に上書きしないもの）
+
+/opt/conda/bin/conda install jupyter -y --quiet
+
+/opt/conda/bin/jupyter serverextension enable --py jupyterlab
+
+# "jupyter-lab" => jupyter-lab立ち上げコマンド
+# "--ip=0.0.0.0" => ip制限なし
+# "--port=8888" => EXPOSE命令で書いたポート番号と合わせる
+# ”--no-browser” => ブラウザを立ち上げない。コンテナ側にはブラウザがないので 。
+# "--allow-root" => rootユーザーの許可。セキュリティ的には良くないので、自分で使うときだけ。
+# "--NotebookApp.token=''" => トークンなしで起動許可。これもセキュリティ的には良くない。
+/opt/conda/bin/jupyter lab --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token='' --notebook-dir=/workdir
