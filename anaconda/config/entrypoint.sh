0f861ad2d8b93b615e8ae4431b492f09d08a9c30
Kitamura <ymyk6602@gmail.com>
2019-09-29 16:23:26 +0900

    fix file owner

--- anaconda/config/entrypoint.sh
+++ anaconda/config/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -eu
+
 # ENTRYPOINT命令はコンテナ起動時に実行するコマンドを指定（基本docker runの時に上書きしないもの）
 
 # "jupyter-lab" => jupyter-lab立ち上げコマンド
@@ -7,4 +10,4 @@
 # ”--no-browser” => ブラウザを立ち上げない。コンテナ側にはブラウザがないので 。
 # "--allow-root" => rootユーザーの許可。セキュリティ的には良くないので、自分で使うときだけ。
 # "--NotebookApp.token=''" => トークンなしで起動許可。これもセキュリティ的には良くない。
-/opt/conda/bin/jupyter lab --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token='' --notebook-dir=/workdir
+/opt/conda/bin/jupyter lab --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token='' --notebook-dir=/home/$1/workdir
