d22afd21e78ce0d030f8b8ff2bfa17dea2d236d4
Kitamura <ymyk6602@gmail.com>
2020-03-22 17:22:23 +0900

    add no gpu

--- engine/build_docker.sh
+++ engine/build_docker.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-docker build -t cpp_engine\
-       --build-arg user=${USER}\
-       --build-arg display=${DISPLAY} .
+has_gpu=`lspci | grep -i nvidia`
+
+if [ -n "${has_gpu}" ]; then
+    docker build -t cpp_engine -f ./Dockerfile\
+           --build-arg user=${USER}\
+           --build-arg display=${DISPLAY} .
+else
+    docker build -t cpp_engine -f ./Dockerfile.no_gpu \
+           --build-arg user=${USER}\
+           --build-arg display=${DISPLAY} .
+fi
--- engine/run_docker.sh
+++ engine/run_docker.sh
@@ -15,16 +15,34 @@ if [ -n "${container_id}" ]; then
     unset container_id
 fi
 
-docker run -t -i -p 33330:22 --name ${container_name} --hostname ${hostname} --gpus all \
-       -v /tmp/.X11-unix:/tmp/.X11-unix  \
-       -v /etc/localtime:/etc/localtime  \
-       -v /etc/passwd:/etc/passwd \
-       -v /etc/shadow:/etc/shadow \
-       -v /etc/group:/etc/group \
-       -v ~/work:/home/${USER}/work \
-       -v ~/dotfiles:/home/${USER}/dotfiles \
-       -v ${source_dir}/config:/home/${USER}/config \
-       -v ~/.gitconfig:/home/${USER}/.gitconfig\
-       -e DISPLAY=${DISPLAY} \
-       -e USER_NAME=${USER} \
-       -d --restart=always ${container_name} /home/${user}/config/entrypoint.sh
+has_gpu=`lspci | grep -i nvidia`
+
+if [ -n "${has_gpu}" ]; then
+    docker run -t -i -p 33330:22 --name ${container_name} --hostname ${hostname} --gpus all \
+           -v /tmp/.X11-unix:/tmp/.X11-unix  \
+           -v /etc/localtime:/etc/localtime  \
+           -v /etc/passwd:/etc/passwd \
+           -v /etc/shadow:/etc/shadow \
+           -v /etc/group:/etc/group \
+           -v ~/work:/home/${USER}/work \
+           -v ~/dotfiles:/home/${USER}/dotfiles \
+           -v ${source_dir}/config:/home/${USER}/config \
+           -v ~/.gitconfig:/home/${USER}/.gitconfig\
+           -e DISPLAY=${DISPLAY} \
+           -e USER_NAME=${USER} \
+           -d --restart=always ${container_name} /home/${user}/config/entrypoint.sh
+else
+    docker run -t -i -p 33330:22 --name ${container_name} --hostname ${hostname} \
+           -v /tmp/.X11-unix:/tmp/.X11-unix  \
+           -v /etc/localtime:/etc/localtime  \
+           -v /etc/passwd:/etc/passwd \
+           -v /etc/shadow:/etc/shadow \
+           -v /etc/group:/etc/group \
+           -v ~/work:/home/${USER}/work \
+           -v ~/dotfiles:/home/${USER}/dotfiles \
+           -v ${source_dir}/config:/home/${USER}/config \
+           -v ~/.gitconfig:/home/${USER}/.gitconfig\
+           -e DISPLAY=${DISPLAY} \
+           -e USER_NAME=${USER} \
+           -d --restart=always ${container_name} /home/${user}/config/entrypoint.sh
+fi
