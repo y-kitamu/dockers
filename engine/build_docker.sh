135b1a720bb7e3ab69a9e8e8ef14ae32ebc9d696
Kitamura <ymyk6602@gmail.com>
2020-03-29 18:59:05 +0900

    refactor engine docker file

--- engine/build_docker.sh
+++ engine/build_docker.sh
@@ -2,11 +2,11 @@
 has_gpu=`lspci | grep -i nvidia`
 
 if [ -n "${has_gpu}" ]; then
-    docker build -t cpp_engine -f ./Dockerfile\
-           --build-arg user=${USER}\
-           --build-arg display=${DISPLAY} .
+    docker build -t cpp_engine_base -f ./Dockerfile.gpu .
 else
-    docker build -t cpp_engine -f ./Dockerfile.no_gpu \
-           --build-arg user=${USER}\
-           --build-arg display=${DISPLAY} .
+    docker build -t cpp_engine_base -f ./Dockerfile.no_gpu .
 fi
+
+docker build -t cpp_engine -f ./Dockerfile\
+    --build-arg user=${USER}\
+    --build-arg display=${DISPLAY} .
--- engine/run_docker.sh
+++ engine/run_docker.sh
@@ -8,7 +8,7 @@ container_name="cpp_engine"
 hostname="cpp-engine"
 user=`id -n -u`
 
-container_id=`docker ps -a --filter ancestor=${image_name} --format "{{.ID}}"`
+container_id=`docker ps -a --filter name=${container_name} --format "{{.ID}}"`
 if [ -n "${container_id}" ]; then
     docker stop ${container_id}
     docker rm ${container_id}
@@ -17,34 +17,22 @@ fi
 
 has_gpu=`lspci | grep -i nvidia`
 
+OPTS=""
 if [ -n "${has_gpu}" ]; then
-    docker run -t -i --cap-add SYS_PTRACE --security-opt "seccomp=unconfined" \
-           -p 33330:22 --name ${container_name} --hostname ${hostname} --gpus all \
-           -v /tmp/.X11-unix:/tmp/.X11-unix  \
-           -v /etc/localtime:/etc/localtime  \
-           -v /etc/passwd:/etc/passwd \
-           -v /etc/shadow:/etc/shadow \
-           -v /etc/group:/etc/group \
-           -v ~/work:/home/${USER}/work \
-           -v ~/dotfiles:/home/${USER}/dotfiles \
-           -v ${source_dir}/config:/home/${USER}/config \
-           -v ~/.gitconfig:/home/${USER}/.gitconfig\
-           -e DISPLAY=${DISPLAY} \
-           -e USER_NAME=${USER} \
-           -d --restart=always ${container_name} /home/${user}/config/entrypoint.sh
-else
-    docker run -t -i --cap-add SYS_PTRACE --security-opt "seccomp=unconfined" \
-           -p 33330:22 --name ${container_name} --hostname ${hostname} \
-           -v /tmp/.X11-unix:/tmp/.X11-unix  \
-           -v /etc/localtime:/etc/localtime  \
-           -v /etc/passwd:/etc/passwd \
-           -v /etc/shadow:/etc/shadow \
-           -v /etc/group:/etc/group \
-           -v ~/work:/home/${USER}/work \
-           -v ~/dotfiles:/home/${USER}/dotfiles \
-           -v ${source_dir}/config:/home/${USER}/config \
-           -v ~/.gitconfig:/home/${USER}/.gitconfig\
-           -e DISPLAY=${DISPLAY} \
-           -e USER_NAME=${USER} \
-           -d --restart=always ${container_name} /home/${user}/config/entrypoint.sh
+    OPTS="${OPTS} --gpus all"
 fi
+    
+docker run -t -i --cap-add SYS_PTRACE --security-opt "seccomp=unconfined" \
+       -p 33330:22 --name ${container_name} --hostname ${hostname} ${OPTS} \
+       -v /tmp/.X11-unix:/tmp/.X11-unix  \
+       -v /etc/localtime:/etc/localtime  \
+       -v /etc/passwd:/etc/passwd \
+       -v /etc/shadow:/etc/shadow \
+       -v /etc/group:/etc/group \
+       -v ~/work:/home/${USER}/work \
+       -v ~/dotfiles:/home/${USER}/dotfiles \
+       -v ${source_dir}/config:/home/${USER}/config \
+       -v ~/.gitconfig:/home/${USER}/.gitconfig\
+       -e DISPLAY=${DISPLAY} \
+       -e USER_NAME=${USER} \
+       -d --restart=always ${container_name} /home/${user}/config/entrypoint.sh
