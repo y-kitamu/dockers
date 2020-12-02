f8a399d0e0e1393d453d28275955f7b47c828b2b
EC2 Default User <ec2-user@ip-10-0-1-174.ap-northeast-1.compute.internal>
2020-12-02 14:42:57 +0000

    wip

--- mlflow/start.sh
+++ mlflow/start.sh
@@ -4,12 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ ! -e ${HOME}/.aws ]; then
-    mkdir ${HOME}/.aws/
-fi
+# if [ ! -e ${HOME}/.aws ]; then
+#     mkdir ${HOME}/.aws/
+# fi
 # cp $AWS_CREDENTIALS ${HOME}/.aws/credentials
 
-mlflow db upgrade $DB_URI
+# mlflow db upgrade $DB_URI
 
 mlflow server \
     --backend-store-uri $DB_URI \
