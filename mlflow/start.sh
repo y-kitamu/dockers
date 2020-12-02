12ffdd317e34290666c5bfe3e1dc5560650a37a5
Kitamura <ymyk6602@gmail.com>
2020-12-02 21:20:57 +0900

    change s3 to local

--- mlflow/start.sh
+++ mlflow/start.sh
@@ -7,7 +7,7 @@ set -o pipefail
 if [ ! -e ${HOME}/.aws ]; then
     mkdir ${HOME}/.aws/
 fi
-cp $AWS_CREDENTIALS ${HOME}/.aws/credentials
+# cp $AWS_CREDENTIALS ${HOME}/.aws/credentials
 
 mlflow db upgrade $DB_URI
 
@@ -15,4 +15,4 @@ mlflow server \
     --backend-store-uri $DB_URI \
     --host 0.0.0.0 \
     --port 80 \
-    --default-artifact-root s3://$AWS_S3_BUCKET
+    --default-artifact-root $ARTIFACTS_PATH
