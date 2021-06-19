bbda468fc2a30bcd76f586b9f13f7b40a8570fc2
Kitamura <ymyk6602@gmail.com>
2021-06-19 16:21:27 +0900

    add jupyter lab to rust docker

--- rust/config/entrypoint.sh
+++ rust/config/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-/usr/sbin/sshd -D
+function run_as_user() {
+    PATH=${HOME}/.cargo/bin${PATH:+:${PATH}} jupyter lab --ip='*' --port=5000 --no-browser --NotebookApp.token='' --notebook-dir=/home/`id -n -u`/work
+}
+
+/usr/sbin/sshd
+RUN_AS_USER=$(declare -f run_as_user)
+sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH} -H -u ${USER_NAME} bash -c "${RUN_AS_USER}; run_as_user"
