cadda9ca64a2e2e26d73e9183774f85077d663a9
Kitamura <ymyk6602@gmail.com>
2021-12-11 08:07:40 +0900

    add node docker

---
+++ node/build_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+CUR_DIR=$(pwd)
+
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+USER=$(id -u ${USER_ID} -n)
+
+cd ${SCRIPT_DIR}
+# docker-compose build --build-arg uid=${USER_ID} --build-arg gid=${GROUP_ID} --build-arg user=${USER}
+docker-compose build \
+               --build-arg uid=${USER_ID} --build-arg gid=${GROUP_ID} --build-arg user=${USER}
+cd ${CUR_DIR}
