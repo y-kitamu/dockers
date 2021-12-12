e156e13b0f84a4bcf1695902e913c9f8f38a6187
Kitamura <ymyk6602@gmail.com>
2021-12-12 11:53:47 +0900

    update node docker

--- node/build_docker.sh
+++ node/build_docker.sh
@@ -6,9 +6,12 @@ CUR_DIR=$(pwd)
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 USER=$(id -u ${USER_ID} -n)
+DISP=${DISPLAY}
+
+echo "USER_ID = ${USER_ID}, GROUP_ID = ${GROUP_ID}, USER = ${USER}, DISP = ${DISP}"
 
 cd ${SCRIPT_DIR}
-# docker-compose build --build-arg uid=${USER_ID} --build-arg gid=${GROUP_ID} --build-arg user=${USER}
 docker-compose build \
-               --build-arg uid=${USER_ID} --build-arg gid=${GROUP_ID} --build-arg user=${USER}
+               --build-arg uid=${USER_ID} --build-arg gid=${GROUP_ID} --build-arg user=${USER} \
+               --build-arg display=${DISP}
 cd ${CUR_DIR}
